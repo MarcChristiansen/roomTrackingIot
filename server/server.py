3ba55b3b17c7a5aa2e690bae740b0bbc9db272a1
Malthe Glent-Madsen <malthe08@hotmail.com>
2021-11-03 17:37:12 +0100

    Made live map template

--- server/server.py
+++ server/server.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/live")
+def live():
+    return render_template("live.html")
+
 def runServer():
     app.run(debug = True, host="0.0.0.0", port=80)
 
