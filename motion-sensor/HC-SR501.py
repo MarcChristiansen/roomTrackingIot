bbee3063c3f4bd42c60e9f7edb7c5178180ca984
Malthe Glent-Madsen <malthe08@hotmail.com>
2021-11-03 11:28:29 +0100

    Added HC-SR501 support

---
+++ motion-sensor/HC-SR501.py
@@ -0,0 +1,14 @@
+import RPi.GPIO as GPIO
+
+class hcsr05Sensor(object):
+    def __init__(self, GPIO_PIR):
+        self.GPIO_PIR = GPIO_PIR
+        GPIO.setup(self.GPIO_PIR, GPIO.IN)
+
+    def get_motion(self):
+        #If HC-SR501 pin is HIGH, motions is detected
+        motion = GPIO.input(self.GPIO_PIR) == 1  
+        return motion
+        
+    def cleanup(self):
+        GPIO.cleanup(self.GPIO_PIR)
