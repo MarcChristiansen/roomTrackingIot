5002e9714754734772402d4090a385c23e169c59
Marc Christiansen <mc@marcs.dk>
2021-11-10 15:17:14 +0100

    refactoring of server

--- compileServer.sh
+++ compileServer.sh
@@ -1,1 +1,4 @@
-docker build -t schollz/find3 find3-server/
+docker build -t schollz/find3 serverside/find3-server-docker/
+docker build -t webserver/rt  serverside/webserver-docker
+docker build -t mqtt/rt 	  serverside/mqtt-server-docker
+
