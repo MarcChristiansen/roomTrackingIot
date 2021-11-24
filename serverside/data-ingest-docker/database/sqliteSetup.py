989ed46f792323334ca776bf7e091083d8db76a3
Marc Christiansen <mc@marcs.dk>
2021-11-24 16:33:41 +0100

    Initial working db

--- serverside/data-ingest-docker/database/sqliteSetup.py
+++ serverside/data-ingest-docker/database/sqliteSetup.py
@@ -9,10 +9,11 @@ def setupDB(name):
     cur.execute('''DROP TABLE IF EXISTS motion''')
     cur.execute('''DROP TABLE IF EXISTS distance''')
 
-    cur.execute('''CREATE TABLE ocupancy (timestamp Integer, location TEXT, occupied INTEGER)''')
+    cur.execute('''CREATE TABLE occupancy (timestamp Integer, location TEXT, occupied INTEGER)''')
     cur.execute('''CREATE TABLE devices  (timestamp Integer, location TEXT, count Integer)''')
+    cur.execute('''CREATE TABLE motion (timestamp Integer, device TEXT, location TEXT, motion Integer)''')
     cur.execute('''CREATE TABLE distance (timestamp Integer, device TEXT, location TEXT, unit TEXT, distance Real)''')
-    cur.execute('''CREATE TABLE distance (timestamp Integer, device TEXT, location TEXT, motion Integer)''')
+
 
     con.commit()
     con.close()
