bb3127274bca7ac04c24c56e24d126cdf66f05a4
Malthe Glent-Madsen <malthe08@hotmail.com>
2021-12-01 16:58:26 +0100

    No testing required

---
+++ serverside/webserver-docker/static/js/living.js
@@ -0,0 +1,33 @@
+const statusElement = document.querySelector("#status");
+
+client = new Paho.Client(location.hostname, 8883, "webClient-" + uuidv4());
+
+client.onConnectionLost = onConnectionLost;
+client.onMessageArrived = onMessageArrived;
+
+client.connect({onSuccess: onConnect, reconnect: true});
+
+function onConnectionLost(response){
+    if(response.errorCode !== 0){
+        console.log("MQTT connection lost: " + response.errorMessage);
+    }
+}
+
+function onConnect() {
+    console.log("Connected to MQTT");
+
+    client.subscribe("dk/ivy/occupancy/living");
+}
+
+function onMessageArrived(message){
+    const payload = JSON.parse(message.payloadString);
+    const occupied = payload.occupied;
+
+    if(occupied) {
+        statusElement.innerHTML = "Active";
+        statusElement.style.color = "green";
+    } else {
+        statusElement.innerHTML = "Inactive";
+        statusElement.style.color = "red";
+    }
+}
